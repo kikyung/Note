0c753f3b66c2aef84a0c46f4417a822e20aa6ec4
kikyung <eddie@rxc.co.kr>
2024-12-27 13:37:52 +0900

    first commit

---
+++ src/main/java/kkk/note/Java2025Application.java
@@ -0,0 +1,13 @@
+package kkk.note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java2025Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Java2025Application.class, args);
+  }
+
+}
---
+++ src/test/java/kkk/note/Java2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package kkk.note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java2025ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
